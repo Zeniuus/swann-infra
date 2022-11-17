4b2c8faae1feec414e4cd203708915bcda6ec9ed
SuhwanJee <suhwan@rapportlabs.kr>
2022-11-17 23:25:50 +0900

    RDB 내리기

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -24,16 +24,16 @@ resource "aws_iam_role" "swann_infra_bastion" {
 EOF
 }
 
-resource "aws_instance" "swann_infra_bastion" {
-  ami           = "ami-0b827f3319f7447c6" // ap-northeast-2
-  instance_type = "t2.micro"
+# resource "aws_instance" "swann_infra_bastion" {
+#   ami           = "ami-0b827f3319f7447c6" // ap-northeast-2
+#   instance_type = "t2.micro"
 
-  iam_instance_profile = aws_iam_instance_profile.swann_infra_bastion.id
+#   iam_instance_profile = aws_iam_instance_profile.swann_infra_bastion.id
 
-  key_name = "swann"
+#   key_name = "swann"
 
-  vpc_security_group_ids = [aws_security_group.swann_infra_bastion.id]
-}
+#   vpc_security_group_ids = [aws_security_group.swann_infra_bastion.id]
+# }
 
 resource "aws_security_group" "swann_infra_bastion" {
   name        = "swann-infra-bastion"
@@ -41,16 +41,16 @@ resource "aws_security_group" "swann_infra_bastion" {
   vpc_id      = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "bastion_to_main_db" {
-  description       = "Allow bastion instance connection"
-  security_group_id = aws_security_group.main_db.id
-
-  type                     = "ingress"
-  from_port                = 3306
-  to_port                  = 3306
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.swann_infra_bastion.id
-}
+#resource "aws_security_group_rule" "bastion_to_main_db" {
+#  description       = "Allow bastion instance connection"
+#  security_group_id = aws_security_group.main_db.id
+#
+#  type                     = "ingress"
+#  from_port                = 3306
+#  to_port                  = 3306
+#  protocol                 = "tcp"
+#  source_security_group_id = aws_security_group.swann_infra_bastion.id
+#}
 
 resource "aws_security_group_rule" "ssh_to_bastion" {
   description       = "Allow SSH connections to bastion instance"
@@ -76,9 +76,9 @@ resource "aws_security_group_rule" "bastion_egress_all" {
 
 resource "aws_key_pair" "swann" {
   key_name   = "swann"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCy+PoTyFYq76ztU/8OwUsH/nNeIwpn/eIGW/cEMZDTeKzvikds1GF6gluymQecuSlFbyp9SeEOPIcE2YllkkFT0Bw/N/Lm9DSGeHzDrXLr8zG3W4MtPEYLElyQu+WIvazqwF2FmDFNOFt41lfHYLyPvem3QX0h8UJYcm1ZJVIryhbfkzoWhmVLmz3TKJWwL5icLgrRwKvr4D9YY9VZnl8ZQ7P0aRECRZkun1I7sUJRd9f9Mq5brz4HkVRtTGrXx7UVQR5M9COylps/bJc1kh7eohvJXISeooedn1Xx4xt39tU1SOQ0rDSG7F9t/uDj5XVWK3bq/PH4aIWcKhGKl9gwkX28OccTn71GLm5LWclPHgLCW+J/709P4tUr2ioN63bCbwJxgk7Vi73Dd/cmovb10QtF2QsD5xo7ZNTdvMSyXfdBPBxIpO6uZN/zlKfb8kvN8n8Cv+yZO7xXjAcDe6WpLsW1kZfRs0kdVqQ+1iLewXva437HwZJEbMasB7eEW58= swann@Suhwanui-MacBookPro.local"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDAbqmVufuZEdLVI4fBKpt0Ajf4LHVFCvg7p1KkwE0Px+XiWqHp3SvPfXhX0MVtvF+sVcqDfPGx062NOHqiN+59OWsVL6mJnSNDxN2sFIyN7bqU2eiNBBoKaa86WlnWKbACl1Wv/LVxWp/16G72MPrWYx593Hg6RvyzrwwQME6wqyQ2Nf2oQjlm8U8yPvQEOK1QFbdCpgqLCg8dFSUlDeLQewPHnrZvHTi5zA4EcdtJqoGSnqa/I7V3qfsh4/E5DPcJL2uioCYCfTgFJWCX+4DeMhYlOtYEUUV6hncfBSLlzxLdp0fx/h0YNXGzKIbloq9WWN5BmXjryade2hTnQ07NGB7IFUu2+hOGQu0pomB6q2pmYT9sH6o98jtv9ejRBFMjOB3qTUROZvPrnVheXiPgZ47OzW2U805fsmEs0clSOl4YsYZJcilHyw5+yhN59QADQxxWFpVLNbQgLa/JOYhrymURiQ0zmRSmC4MM/amcPWjNbHBXYGQuQPSsD0JQMS0= suhwan@ip-172-16-12-170.ap-northeast-2.compute.internal"
 }
 
-output "swann_infra_bastion_dns" {
-  value = aws_instance.swann_infra_bastion.public_dns
-}
+# output "swann_infra_bastion_dns" {
+#   value = aws_instance.swann_infra_bastion.public_dns
+# }
